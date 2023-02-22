37f213f6c8e050e30e5ed37f10affbb6fa3ecbe3
blitty-codes <blittynet@gmail.com>
2023-02-22 21:31:29 +0100

    Added README & Deleted unnecesary

---
+++ README.md
@@ -0,0 +1,34 @@
+## Nvim configuration
+
+1. Install (nvim)[https://neovim.io/] if possible v0.9 or v0.8.
+2. Copy `nvim` folder to `$PATH/.config`.
+3. Install a (NerdFont)[https://www.nerdfonts.com/font-downloads], my favourite: **FiraCode NF**.
+4. Set the installed font on your terminal.
+5. Enjoy!
+
+### Commands
+- `<leader>` = <space>
+
+#### NvimTree
+|          |        |
+|----------|--------|
+| <c-n>    | Open/Close file explorer |
+| <c-f>    | Focus file explorer |
+
+#### Autocompletion
+|          |        |
+|----------|--------|
+| <Ctrl-n> | Select next item |
+| <Ctrl-p> | Select prev item |
+| <Ctrl-M-k> | Scroll docs (up) |
+| <Ctrl-space> | Show all table |
+
+#### Telescope
+On normal mode
+
+|          |        |
+|----------|--------|
+| <leader>ff | Find files |
+| <leader>fg | Live grep |
+| <leader>fb | Show opened buffers (tabs) |
+| <leader>fh | Builtin helper tags |
--- nvim/lua/core/plugin_config/auto-completion.lua
+++ nvim/lua/core/plugin_config/auto-completion.lua
@@ -110,76 +110,12 @@ local SOURCES_RELATED_CONFIGURATION = {
 }
 
 cmp.setup({
-  snippet =  {
-    expand = function(args)
-      -- For `vsnip`, uncomment the following.
-      -- vim.fn["vsnip#anonymous"](args.body)
-      -- For `luasnip`, uncomment the following.
-      -- require('luasnip').lsp_expand(args.body)
-      -- For snippy, uncomment the following.
-      -- require('snippy').expand_snippet(args.body)
-      -- For `ultisnips`
-      vim.fn["UltiSnips#Anon"](args.body)
-    end,
-  },-- SNIPPET_CONFIGURATION,
-  formatting = {
-    format = function(entry, vim_item)
-      -- Kind icons
-      vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) --Concatonate the icons with name of the item-kind
-      vim_item.menu = ({
-        nvim_lsp = "[LSP]",
-        spell = "[Spellings]",
-        zsh = "[Zsh]",
-        buffer = "[Buffer]",
-        ultisnips = "[Snip]",
-        treesitter = "[Treesitter]",
-        calc = "[Calculator]",
-        nvim_lua = "[Lua]",
-        path = "[Path]",
-        nvim_lsp_signature_help = "[Signature]",
-        cmdline = "[Vim Command]"
-      })[entry.source.name]
-      return vim_item
-    end,
-  }, -- FORMATTING,
-  mapping = {
-    ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-    ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-    ['<C-M-k>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-M-j>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable,
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-  }, -- MAPPING,
-  completion = {
-    keyword_length = 1,
-  }, -- COMPLETION_RELATED_CONFIGURATION,
-  matching = {
-    disallow_fuzzy_matching = false,
-  },-- FUZZY_MATCHING,
-  sources = cmp.config.sources(
-    {
-      { name = 'nvim_lsp' },
-      -- For ultisnips users
-      { name = 'ultisnips' },
-      -- For vsnip users, uncomment the following.
-      -- { name = 'vsnip' },
-      -- For luasnip users, uncomment the following.
-      -- { name = 'luasnip' },
-      -- For snippy users, uncomment the following.
-      -- { name = 'snippy' },
-    }, {
-      { name = 'buffer' },
-    }, {
-      { name = 'nvim_lsp_signature_help' },
-    }, {
-      { name = 'path' },
-    }
-  )-- SOURCES_RELATED_CONFIGURATION)
+  snippet =  SNIPPET_CONFIGURATION,
+  formatting = FORMATTING,
+  mapping = MAPPING,
+  completion = COMPLETION_RELATED_CONFIGURATION,
+  matching = FUZZY_MATCHING,
+  sources = cmp.config.sources(SOURCES_RELATED_CONFIGURATION)
 })
 
 
